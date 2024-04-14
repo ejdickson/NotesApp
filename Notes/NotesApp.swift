22d7ff14cc53a59596c283d64f625e15188e16b6
Emma Brennan <ejdickson93@gmail.com>
2024-04-14 09:30:23 -0400

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Emma Brennan on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
